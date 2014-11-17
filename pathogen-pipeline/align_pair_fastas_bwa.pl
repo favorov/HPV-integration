fff78e6d7bd7f85295d967d026d9359d542aafc8
Alexander Favorov <favorov@sensi.org>
2014-11-17 17:03:48 -0500

    bwa returns non-zero

--- pathogen-pipeline/align_pair_fastas_bwa.pl
+++ pathogen-pipeline/align_pair_fastas_bwa.pl
@@ -21,8 +21,8 @@ my $output_bam_unsorted_file = $output_bam_unsorted_file_prefix.".bam";
 $sai_1_file = $out_prefix.".01.sai";
 $sai_2_file = $out_prefix.".02.sai";
 
-system "bwa aln -l 32 -k 2 -t 4 $input_fasta_file -1 $input_fq_01_file > $sai_1_file" or die;
-system "bwa aln -l 32 -k 2 -t 4 $input_fasta_file -2 $input_fq_02_file > $sai_2_file" or die;
+system "bwa aln -l 32 -k 2 -t 4 $input_fasta_file -1 $input_fq_01_file > $sai_1_file";
+system "bwa aln -l 32 -k 2 -t 4 $input_fasta_file -2 $input_fq_02_file > $sai_2_file";
 system "bwa sampe $input_fasta_file $sai_1_file $sai_2_file $input_fq_01_file $input_fq_02_file| samtools view -Shb - > $output_bam_unsorted_file" or die;
 system "samtools sort $output_bam_unsorted_file $out_prefix" or die;
 system "samtools index $output_bam_file" or die;
