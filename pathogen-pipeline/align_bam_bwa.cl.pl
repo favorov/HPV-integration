1ac231d74e3a008e5e876c2ba5c008241c40de41
Alexander Favorov <favorov@sensi.org>
2014-11-18 22:09:25 -0500

    added () around system $var calls; added code not to rum big alingment if the result is in folder

--- pathogen-pipeline/align_bam_bwa.cl.pl
+++ pathogen-pipeline/align_bam_bwa.cl.pl
@@ -19,19 +19,23 @@ my $output_bam_unsorted_file = $output_bam_unsorted_file_prefix.".bam";
 
 my $output_bam_file = $out_prefix.".bam";
 
+if (-f $output_bam_file and -s $output_bam_file and -f "$output_bam_file.bai" and -s "$output_bam_file.bai")
+{
+	print "$output_bam_file $output_bam_file.bai exist, we do not run fasta alignment.\nIf it is an error, remove them manually before start.\n";
+	exit 0;
+}
+
 $sai_1_file = $out_prefix.".01.sai";
 
-#system "bwa aln -l 32 -k 2 -t 4 $input_fasta_file -b $input_bam_file > $sai_1_file" if (not -f $sai_1_file or not -s $sai_1_file);
-#system "bwa samse $input_fasta_file $sai_1_file $input_bam_file | samtools view  -Shb -F4 - > $output_bam_unsorted_file" if (not -f $output_bam_unsorted_file or not -s $output_bam_unsorted_file);
-#print "samtools sort\n"
-#system "samtools sort $output_bam_unsorted_file $out_prefix" if (not -f $output_bam_file or not -s $output_bam_file);
-#print "samtools index\n"
-#system "samtools index $output_bam_file" or die;
+#bwa aln -l 32 -k 2 -t 4 $input_fasta_file -b $input_bam_file > $sai_1_file
+#bwa samse $input_fasta_file $sai_1_file $input_bam_file | samtools view  -Shb -F4 - > $output_bam_unsorted_file
+#samtools sort $output_bam_unsorted_file $out_prefix
+#samtools index $output_bam_file
 
 if (not -f $sai_1_file or not -s $sai_1_file)
 {
 	my $fasta1_run="bwa aln -l 32 -k 2 -t 4 $input_fasta_file -b $input_bam_file > $sai_1_file";
-	system $fasta1_run == 0 
+	system ($fasta1_run) == 0 
 		or die "system $fasta1_run died: $?";
 }
 
@@ -39,7 +43,7 @@ if (not -f $sai_1_file or not -s $sai_1_file)
 if (not -f $output_bam_unsorted_file or not -s $output_bam_unsorted_file)
 {
 	my $fasta_combine_run="bwa samse $input_fasta_file $sai_1_file $input_bam_file | samtools view  -Shb -F4 - > $output_bam_unsorted_file";
-	system $fasta_combine_run == 0 
+	system ($fasta_combine_run) == 0 
 		or die "system $fasta_combine_run died: $?";
 }
 
@@ -47,7 +51,7 @@ if (not -f $output_bam_file or not -s $output_bam_file)
 {
 	print "samtools sort\n";
 	my $samtools_sort_run="samtools sort $output_bam_unsorted_file $out_prefix";
-	system $samtools_sort_run == 0
+	system ($samtools_sort_run) == 0
 		or die "system $samtools_sort_run died: $?";
 }
 
@@ -55,7 +59,7 @@ if (not -f "$output_bam_file.bai" or not -s "$output_bam_file.bai")
 {
 	print "samtools index\n";
 	my $samtools_index_run="samtools index $output_bam_file";
-	system $samtools_index_run == 0 
+	system ($samtools_index_run) == 0 
 		or die "system $samtools_index_run died : $?";
 }
 
