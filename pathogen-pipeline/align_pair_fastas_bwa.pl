378a5c73fdbae24f28f40d0b7bb3e707b5e6f0ae
Alexander Favorov <favorov@sensi.org>
2014-11-18 22:07:02 -0500

    added () around system $var calls; added code not to rum big alingment if the result is in folder

--- pathogen-pipeline/align_pair_fastas_bwa.pl
+++ pathogen-pipeline/align_pair_fastas_bwa.pl
@@ -18,27 +18,33 @@ $output_bam_file = $out_prefix.".bam";
 my $output_bam_unsorted_file_prefix = $out_prefix.".unsorted";
 my $output_bam_unsorted_file = $output_bam_unsorted_file_prefix.".bam";
 
+if (-f $output_bam_file and -s $output_bam_file and -f "$output_bam_file.bai" and -s "$output_bam_file.bai")
+{
+	print "$output_bam_file $output_bam_file.bai exist, we do not run fasta alignment.\nIf it is an error, remove them manually before start.\n";
+	exit 0;
+}
+
 $sai_1_file = $out_prefix.".01.sai";
 $sai_2_file = $out_prefix.".02.sai";
 
 if (not -f $sai_1_file or not -s $sai_1_file)
 {
 	my $fasta1_run="bwa aln -l 32 -k 2 -t 4 $input_fasta_file -1 $input_fq_01_file > $sai_1_file";
-	system $fasta1_run == 0 
+	system ($fasta1_run) == 0 
 		or die "system $fasta1_run died: $?";
 }
 
 if (not -f $sai_2_file or not -s $sai_2_file)
 {
 	my $fasta2_run="bwa aln -l 32 -k 2 -t 4 $input_fasta_file -2 $input_fq_02_file > $sai_2_file";
-	system $fasta2_run == 0 
+	system ($fasta2_run) == 0 
 		or die "system $fasta2_run died: $?";
 }
 
 if (not -f $output_bam_unsorted_file or not -s $output_bam_unsorted_file)
 {
 	my $fasta_combine_run="bwa sampe $input_fasta_file $sai_1_file $sai_2_file $input_fq_01_file $input_fq_02_file | samtools view -Shb - > $output_bam_unsorted_file";
-	system $fasta_combine_run == 0 
+	system ($fasta_combine_run) == 0 
 		or die "system $fasta_combine_run died: $?";
 }
 
@@ -46,7 +52,7 @@ if (not -f $output_bam_file or not -s $output_bam_file)
 {
 	print "samtools sort\n";
 	my $samtools_sort_run="samtools sort $output_bam_unsorted_file $out_prefix";
-	system $samtools_sort_run == 0
+	system ($samtools_sort_run) == 0
 		or die "system $samtools_sort_run died: $?";
 }
 
@@ -54,7 +60,7 @@ if (not -f "$output_bam_file.bai" or not -s "$output_bam_file.bai")
 {
 	print "samtools index\n";
 	my $samtools_index_run="samtools index $output_bam_file";
-	system $samtools_index_run == 0 
+	system ($samtools_index_run) == 0 
 		or die "system $samtools_index_run died : $?";
 }
 
